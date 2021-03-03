06d3e1e54d406a09323a4e71bcc757d185b7efa0
ProgrammerZT <programmerwzt@gmail.com>
2021-03-03 18:10:51 +0800

    update line bar

--- js/index.js
+++ js/index.js
@@ -89,3 +89,488 @@
     myChart.resize();
   });
 })();
+
+(function () {
+  var myColor = ["#1089E7", "#F57474", "#56D0E3", "#F8B448", "#8B78F6"];
+  // 1. 实例化对象
+  var myChart = echarts.init(document.querySelector(".bar2 .chart"));
+  // 2. 指定配置和数据
+  var option = {
+    grid: {
+      top: "10%",
+      left: "22%",
+      bottom: "10%",
+      // containLabel: true
+    },
+    // 不显示x轴的相关信息
+    xAxis: {
+      show: false,
+    },
+    yAxis: [
+      {
+        type: "category",
+        inverse: true,
+        data: ["HTML5", "CSS3", "Javascript", "VUE", "REACT"],
+        // 不显示y轴的线
+        axisLine: {
+          show: false,
+        },
+        // 不显示刻度
+        axisTick: {
+          show: false,
+        },
+        // 把刻度标签里面的文字颜色设置为白色
+        axisLabel: {
+          color: "#fff",
+        },
+      },
+      {
+        data: [702, 350, 610, 793, 664],
+        inverse: true,
+        // 不显示y轴的线
+        axisLine: {
+          show: false,
+        },
+        // 不显示刻度
+        axisTick: {
+          show: false,
+        },
+        // 把刻度标签里面的文字颜色设置为白色
+        axisLabel: {
+          color: "#fff",
+        },
+      },
+    ],
+    series: [
+      {
+        name: "条",
+        type: "bar",
+        data: [70, 34, 60, 78, 69],
+        yAxisIndex: 0,
+        // 修改第一组柱子的圆角
+        itemStyle: {
+          barBorderRadius: 20,
+          // 此时的color 可以修改柱子的颜色
+          color: function (params) {
+            // params 传进来的是柱子对象
+            // console.log(params);
+            // dataIndex 是当前柱子的索引号
+            return myColor[params.dataIndex];
+          },
+        },
+        // 柱子之间的距离
+        barCategoryGap: 50,
+        //柱子的宽度
+        barWidth: 10,
+        // 显示柱子内的文字
+        label: {
+          show: true,
+          position: "inside",
+          // {c} 会自动的解析为 数据  data里面的数据
+          formatter: "{c}%",
+        },
+      },
+      {
+        name: "框",
+        type: "bar",
+        barCategoryGap: 50,
+        barWidth: 15,
+        yAxisIndex: 1,
+        data: [100, 100, 100, 100, 100],
+        itemStyle: {
+          color: "none",
+          borderColor: "#00c1de",
+          borderWidth: 3,
+          barBorderRadius: 15,
+        },
+      },
+    ],
+  };
+
+  // 3. 把配置给实例对象
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+
+(function () {
+  const YearData = [
+    {
+      year: "2020", // 年份
+      data: [
+        // 两个数组是因为有两条线
+        [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+        [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79],
+      ],
+    },
+    {
+      year: "2021", // 年份
+      data: [
+        // 两个数组是因为有两条线
+        [123, 175, 112, 197, 121, 67, 98, 21, 43, 64, 76, 38],
+        [143, 131, 165, 123, 178, 21, 82, 64, 43, 60, 19, 34],
+      ],
+    },
+  ];
+  const myChart = echarts.init(document.querySelector(".line .chart"));
+  const option = {
+    tooltip: {
+      trigger: "axis",
+    },
+    legend: {
+      textStyle: {
+        color: "#4c9bfd", // 图例文字颜色
+      },
+      data: ["新增从业", "新增就业"],
+      right: 10,
+      top: 0,
+    },
+    grid: {
+      top: 20,
+      bottom: 20,
+      // containLabel: true,
+      show: true, // 显示边框
+      borderColor: "#012f4a", // 边框颜色
+    },
+    xAxis: {
+      type: "category",
+      data: [
+        "1月",
+        "2月",
+        "3月",
+        "4月",
+        "5月",
+        "6月",
+        "7月",
+        "8月",
+        "9月",
+        "10月",
+        "11月",
+        "12月",
+      ],
+      axisTick: {
+        show: false, // 去除刻度线
+      },
+      axisLabel: {
+        color: "#4c9bfd", // 文本颜色
+      },
+      axisLine: {
+        show: false, // 去除轴线
+      },
+      boundaryGap: false, // 去除轴内间距
+    },
+    yAxis: {
+      type: "value",
+      axisTick: {
+        show: false, // 去除刻度
+      },
+      axisLabel: {
+        color: "#4c9bfd", // 文字颜色
+      },
+      splitLine: {
+        lineStyle: {
+          color: "#012f4a", // 分割线颜色
+        },
+      },
+    },
+    color: ["#00f2f1", "#ed3f35"],
+    series: [
+      {
+        name: "新增就业",
+        data: YearData[0].data[0],
+        type: "line",
+        smooth: true,
+      },
+      {
+        name: "新增从业",
+        data: YearData[0].data[1],
+        type: "line",
+        smooth: true,
+      },
+    ],
+  };
+
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+
+  //5.添加点击事件
+  const links = document.querySelectorAll(".line a");
+  links.forEach((item, index) => {
+    item.onclick = function () {
+      console.log(index);
+      option.series[0].data = YearData[index].data[0];
+      option.series[1].data = YearData[index].data[1];
+      //重新渲染一次
+      myChart.setOption(option);
+    };
+  });
+})();
+
+(function () {
+  const myChart = echarts.init(document.querySelector(".line2 .chart"));
+  const option = {
+    tooltip: {
+      trigger: "axis",
+    },
+    legend: {
+      data: ["REACT", "VUE"],
+      top: "0%",
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12",
+      },
+    },
+    grid: {
+      left: "10",
+      top: "30",
+      right: "10",
+      bottom: "10",
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        data: [
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "26",
+          "28",
+          "29",
+          "30",
+        ],
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12,
+          },
+        },
+        // x轴线的颜色为   rgba(255,255,255,.2)
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.2)",
+          },
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12,
+          },
+        },
+        // 修改分割线的颜色
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+      },
+    ],
+    series: [
+      {
+        name: "REACT",
+        type: "line",
+        smooth: true,
+        lineStyle: {
+          color: "orange",
+        },
+        // 设置拐点 小圆点
+        symbol: "circle",
+        // 拐点大小
+        symbolSize: 5,
+        // 设置拐点颜色以及边框
+        itemStyle: {
+          color: "#00d887",
+          borderColor: "rgba(221, 220, 107, .1)",
+          borderWidth: 12,
+        },
+        // 开始不显示拐点， 鼠标经过显示
+        showSymbol: false,
+        stack: "总量",
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: "rgba(0, 216, 135, 0.4)",
+              },
+              {
+                offset: 0.8,
+                color: "rgba(0, 216, 135, 0.1)",
+              },
+            ],
+            false
+          ),
+        },
+        emphasis: {
+          focus: "series",
+        },
+        data: [
+          130,
+          10,
+          20,
+          40,
+          30,
+          40,
+          80,
+          60,
+          20,
+          40,
+          90,
+          40,
+          20,
+          140,
+          30,
+          40,
+          130,
+          20,
+          20,
+          40,
+          80,
+          70,
+          30,
+          40,
+          30,
+          120,
+          20,
+          99,
+          50,
+          20,
+        ],
+      },
+      {
+        name: "VUE",
+        type: "line",
+        lineStyle: {},
+        smooth: true,
+        // 单独修改线的样式
+        lineStyle: {
+          color: "#0184d5",
+          width: 2,
+        },
+        // 填充区域
+        areaStyle: {
+          // 渐变色，只需要复制即可
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: "rgba(1, 132, 213,0)", // 渐变色的起始颜色
+              },
+              {
+                offset: 0.8,
+                color: "rgba(1, 132, 213,1)", // 渐变线的结束颜色
+              },
+            ],
+            false
+          ),
+          shadowColor: "rgba(0, 0, 0, 0.1)",
+        },
+        // 设置拐点 小圆点
+        symbol: "circle",
+        // 拐点大小
+        symbolSize: 8,
+        // 设置拐点颜色以及边框
+        itemStyle: {
+          color: "#0184d5",
+          borderColor: "rgba(221, 220, 107, .1)",
+          borderWidth: 12,
+        },
+        // 开始不显示拐点， 鼠标经过显示
+        showSymbol: false,
+        stack: "总量",
+        areaStyle: {},
+        emphasis: {
+          focus: "series",
+        },
+        // series  第一个对象data数据
+        data: [
+          30,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          60,
+          20,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          60,
+          20,
+          40,
+          30,
+          40,
+          30,
+          40,
+          30,
+          40,
+          20,
+          60,
+          50,
+          40,
+        ],
+      },
+    ],
+  };
+
+  myChart.setOption(option);
+
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
